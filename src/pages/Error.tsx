cd07c37a616148fe85398476753f2583a96fdb38
nabil-y <nabil.yassine.dev@gmail.com>
2022-06-25 00:31:08 +0200

    Complete Error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,9 +1,25 @@
+import { Link } from "react-router-dom";
+import Card from "../components/UI/Card";
+
 /**
  * Error function
  * @returns Error page component
  */
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <main>
+      <Card className="max-w-[640px] text-center sm:my-8">
+        <h1 className="pb-4 text-6xl font-bold">ERROR</h1>
+        <p className="pb-4 text-xl">No content found here</p>
+        <Link
+          to="/"
+          className="text-xl font-bold text-secondary underline transition-all hover:text-primary"
+        >
+          Home
+        </Link>
+      </Card>
+    </main>
+  );
 };
 
 export default Error;
